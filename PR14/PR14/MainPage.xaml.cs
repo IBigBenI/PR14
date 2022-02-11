ca544862683fc5ef47ef3fb315470dfc393c4090
Даниил Творогов <tvorogov.dan9@mail.ru>
2022-02-11 22:11:22 +0300

    Добавьте файлы проекта.

---
+++ PR14/PR14/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PR14
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Page_2());
+        }
+    }
+}
---
+++ PR14/PR14/Page_2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PR14
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page_2 : ContentPage
+    {
+        public Page_2()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
